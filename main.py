baeb87406f1cf121ffc20e13e940aab853290104
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-11-09 11:34:27 -0600

    Updated files

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ print("Starting Spotify Downloader")
 print("Hello? Anyone there?", flush=True)
 schedule.every().hour.at(":00").do(query)
 schedule.every().hour.at(":15").do(query)
-schedule.every().hour.at(":30").do(query)
+schedule.every().hour.at(":33").do(query)
 schedule.every().hour.at(":45").do(query)
 # schedule.every().hour.at(":57").do(mongo_to_postgres)
 
